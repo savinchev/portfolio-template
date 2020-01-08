967af083162f10f50e033dcfd0784919aec8c266
Le8wa-dev <savinchev@list.ru>
2020-01-08 18:09:24 +0300

    added adaptive

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -3893,7 +3893,30 @@ $(function() {
         slidesToShow: 2,
         slidesToScroll: 2,
         prevArrow: '<button class="portfolio__arrow  portfolio__arrow--prev" type="button"><svg class="portfolio__arrow-icon"><use xlink:href="#arrow"></use></svg></button>',
-        nextArrow: '<button class="portfolio__arrow  portfolio__arrow--next" type="button"><svg class="portfolio__arrow-icon"><use xlink:href="#arrow"></use></svg></button>'
+        nextArrow: '<button class="portfolio__arrow  portfolio__arrow--next" type="button"><svg class="portfolio__arrow-icon"><use xlink:href="#arrow"></use></svg></button>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    variableWidth: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: "unslick"
+            }
+        ]
     });
 
 });
